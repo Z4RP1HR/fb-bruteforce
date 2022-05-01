d9f78270e4038f29b8a590a776cb4836e7888782
Z4RP1HR <103745705+Z4RP1HR@users.noreply.github.com>
2022-05-01 23:54:29 +0800

    Add files via upload

---
+++ fbbruteforce.py
@@ -0,0 +1,22 @@
+#scam motherfuckas
+print("SKIDESEC FB BRUTEFORCE\n")
+
+name = input("please enter the fb profile link: ")
+print("\nbrute forcing fb")
+input("\ndo you want to speed up hacking? (y/n) ")
+
+print("\ntargeting:" + name + "\n")
+
+import time
+time.sleep(0.9)
+
+for x in range(300):
+  print("bruteforce: " + name)
+  
+print("\nattack succesfully")
+
+input("\ndo you want to see the results? (y/n) ")
+
+for y in range(500):
+    print("this is a prankkkk hehehehehe")
+
